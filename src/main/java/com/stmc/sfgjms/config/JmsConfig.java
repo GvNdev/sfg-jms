f748f4a3d4adfb20efd6b858a981b158f1bd64bd
Gabriel Valdivieso <gabriel.valdivieso.005@gmail.com>
2026-02-07 23:28:57 -0500

    Section 35 - Class 501: Message Converter Configuration

---
+++ src/main/java/com/stmc/sfgjms/config/JmsConfig.java
@@ -0,0 +1,24 @@
+package com.stmc.sfgjms.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.JacksonJsonMessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+/**
+ * Created by: GvN
+ * Date: 07-Feb-26
+ * Time: 23:21
+ * Project Name: sfg-jms
+ */
+@Configuration
+public class JmsConfig {
+    @Bean
+    public MessageConverter messageConverter() {
+        JacksonJsonMessageConverter converter = new JacksonJsonMessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+}
